ba6c23e0e72322d136cf2c64c0318146daf576ea
felipeanfepe <felipeanfepe@gmail.com>
2022-10-19 07:38:09 -0500

    listar facturas

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/ClienteServiceImpl.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/ClienteServiceImpl.java
@@ -3,7 +3,9 @@ package com.bolsadeideas.springboot.backend.apirest.models.services;
 import java.util.List;
 
 import com.bolsadeideas.springboot.backend.apirest.models.dao.IClienteDao;
+import com.bolsadeideas.springboot.backend.apirest.models.dao.IFacturaDao;
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
+import com.bolsadeideas.springboot.backend.apirest.models.entity.Factura;
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Region;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +20,9 @@ public class ClienteServiceImpl implements IClienteService {
     @Autowired
     private IClienteDao clienteDao;
 
+    @Autowired
+    private IFacturaDao facturaDao;
+
     @Override
     @Transactional(readOnly = true)
     public List<Cliente> findAll() {
@@ -29,7 +34,7 @@ public class ClienteServiceImpl implements IClienteService {
     public Page<Cliente> findAll(Pageable pageable) {
         return clienteDao.findAll(pageable);
     }
-    
+
     @Override
     @Transactional(readOnly = true)
     public Cliente findById(Long id) {
@@ -53,5 +58,23 @@ public class ClienteServiceImpl implements IClienteService {
     public List<Region> findAllRegiones() {
         return clienteDao.findAllRegiones();
     }
-    
+
+    @Override
+    @Transactional(readOnly = true)
+    public Factura findFacturaById(Long id) {
+      return facturaDao.findById(id).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public Factura saveFactura(Factura factura) {
+      return facturaDao.save(factura);
+    }
+
+    @Override
+    @Transactional
+    public void deleteFacturaById(Long id) {
+      facturaDao.deleteById(id);
+    }
+
 }
